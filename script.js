8a3f399e7675712e7ec1b11d0fdf0d29aec12eb6
Matluma0 <guillet.lucas13@gmail.com>
2026-06-23 01:19:48 +0200

    menu burger pour portable off

--- script.js
+++ script.js
@@ -4,20 +4,3 @@ window.addEventListener("load", function() {
     document.querySelector(".contenu").style.opacity = "1";
 });
 
-// Menu hamburger
-const hamburger = document.getElementById('hamburger');
-const navMenu = document.getElementById('navMenu');
-
-hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('active');
-    navMenu.classList.toggle('active');
-});
-
-// Ferme le menu quand on clique sur un lien
-const navLinks = navMenu.querySelectorAll('a');
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    });
-});
